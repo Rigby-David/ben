206cb6f6df4ac897cbd5a94af517449e9ef19e72
David <Rigby335@hotmail.com>
2023-12-24 09:51:53 -0800

    feat: cenetered header contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import About from "./components/about/About";
 import Header from "./components/header/Header";
 function App() {
   return (
     <>
       <Header />
+      <div className="main">
+        <About />
+      </div>
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,22 +3,24 @@ import "./Header.css";
 export default function Header() {
   return (
     <header>
-      <div className="header-col-1">
-        <div className="col-1-contents">
-          <div className="col-1-title">
-            <h3>Ben Fisher</h3>
-          </div>
-          <div className="col-1-desc">
-            <p>Technical Writer, User Advocate, Process Nerd</p>
+      <div className="header-columns">
+        <div className="header-col-1">
+          <div className="col-1-contents">
+            <div className="col-1-title">
+              <h3>Ben Fisher</h3>
+            </div>
+            <div className="col-1-desc">
+              <p>Technical Writer, User Advocate, Process Nerd</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="header-col-2">
-        <nav className="nav-container">
-          <a href="">About</a>
-          <a href="">Resume</a>
-          <a href="">Portfolio</a>
-        </nav>
+        <div className="header-col-2">
+          <nav className="nav-container">
+            <a href="">About</a>
+            <a href="">Resume</a>
+            <a href="">Portfolio</a>
+          </nav>
+        </div>
       </div>
     </header>
   );
